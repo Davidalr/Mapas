576737eb8877b8320375193ea07aa1c5fff0e49f
davidalr <david.cadc@gmail.com>
2016-10-11 15:13:04 -0500

    theme

---
+++ wp-content/themes/Mapas/index.php
@@ -0,0 +1,62 @@
+<?php get_header(); ?>
+<section class="row middle Home">
+    <article class="col-6 small-12">
+        <figure class="Home-prize"><img
+                src="<?php bloginfo('template_url') ?>/assets/images/premio-nacional-de-teatro.png" alt="">
+        </figure>
+        <p class="Home-p">
+
+        </p>
+        <ul class="row Date">
+            <li class="col-4 small-12">
+                <h3>Convocatoría</h3>
+                <span></span>
+            </li>
+            <li class="col-4 small-12">
+                <h3>Aviso a finalistas. </h3>
+                <span>Noviembre 10 de 2016.</span>
+            </li>
+            <li class="col-4 small-12">
+                <h3>Gala de premiación</h3>
+                <span>24 de Noviembre de 2016 en el Teatro Colón.</span>
+            </li>
+        </ul>
+    </article>
+    <article class="col-6 small-12">
+        <figure><img src="<?php bloginfo('template_url') ?>/assets/images/reconocer.png" alt=""></figure>
+
+        <div style="text-align: left"><h3>CATEGORÍAS</h3></div>
+
+        <h4>Categorías generales:</h4>
+        <p class="Home-p">. </p>
+
+        <div class=" row around">
+            <ul class="Home-categories">
+                <li>~Mejor obra de teatro</li>
+                <li>~Mejor director </li>
+                <li>~Mejor dramaturgia</li>
+                <li>~Mejor diseño escenografia</li>
+                <li>~Mejor diseño de maquillaje</li>
+            </ul>
+            <ul class="Home-categories ">
+                <li>~Mejor diseño de vestuario</li>
+                <li>~Mejor iluminación</li>
+                <li>~Mejor diseño de sonido</li>
+                <li>~Mejor actriz</li>
+                <li>~Mejor actor</li>
+            </ul>
+        </div>
+        <hr>
+        <h4>Premio Teatro Colón:</h4>
+        <p class="Home-p">Invitamos a grupos constituidos y uniones temporales a participar con proyectos que no hayan sido
+            estrenados. El ganador será escogido por el jurado y por el director del Teatro Colón, de acuerdo a las
+            bases de la convocatoria para una coproducción con el Teatro Colón.</p>
+        <hr>
+        <h4>Categorías de reconocimiento:</h4>
+        <p class="Home-p">Los ganadores serán elegidos por el comité organiazador.</p>
+        <span>Teatro y territorio // Vida y obra // Mejor programa de formación teatral</span>
+
+    </article>
+</section>
+
+<?php get_footer(); ?>
